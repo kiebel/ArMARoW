d792647a60bbf8b2551bd560a8fcb60b15c0ebb4
Christoph Steup <christoph.steup@st.ovgu.de>
2011-06-07 16:06:58 +0200

    adds software CRC implementation
    	- currently CRC ITU_T 16Bit

---
+++ include/armarow/common/crc.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <boost/mpl/int.hpp>
+
+namespace armarow{
+	namespace common{
+		struct CRC{ 
+			struct Polynomials{
+				typedef boost::mpl::int_<0x1021> ITU_T;
+			};
+			
+			template<typename Polynom>
+			static uint16_t calculateCRC( uint8_t* data, uint8_t length ){
+				uint16_t crc = 0;
+
+				for(uint8_t i=0;i<length-2;i++){
+					crc ^= ( (uint16_t) data[i] << 8);
+
+					for (uint8_t j=0; j<8; j++){
+						if( crc & 0x8000 )
+							crc = (crc << 1) ^ Polynom::value;
+						else
+							crc <<= 1;
+					}
+				}
+
+				return crc;
+			}
+		};
+	};
+};
