c0d7ac76563fa280292f16ed822cc23854326351
Thomas Kiebel <tomuchan76@gmail.com>
2011-11-10 20:19:44 +0100

    refactoring phase 1

--- include/armarow/mac/util.h
+++ include/armarow/mac/util.h
@@ -1,11 +1,7 @@
-
 #include <stdlib.h>
 
 template <int number_of_fillbytes>
-struct MessageLossMeasurement{
-
-	uint32_t global_sequence_number;
-	char fillbytes[number_of_fillbytes];   //intended for measurement with different messages sizes
-
+struct MessageLossMeasurement {
+    uint32_t global_sequence_number;
+	char fillbytes[number_of_fillbytes];
 };
-
