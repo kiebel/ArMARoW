68c1d3b7a2dfbb88b368653b30c6a1b054e72fc6
Christoph Steup <christoph.steup@st.ovgu.de>
2011-11-29 08:40:45 +0100

    API draft for mac layer
    	api definition in interface class
    	common error definition
    	mac_bubbler example as proof-of-concept

--- examples/mac_examples/mac_bubbler.cc
+++ examples/mac_examples/mac_bubbler.cc
@@ -1,117 +1,85 @@
 
-#include <armarow/mac/ieee_802.15.4.h>
+#include "armarow/mac/mediumAccessLayerInterface.h"
+#include <avr-halib/share/interruptLock.h>
 
-/* === globals ============================================================== */
-/*! \brief Configuration class for the Mac Layer. We only have to set parameters that should have different values than the default values. */
-struct My_MAC_Config : public armarow::mac::configuration::DefaultMACConfiguration {
-    enum {
-        channel=3,
-        mac_adress_of_node=45
-    };
+using armarow::common::Error;
+
+struct TestMAC : public armarow::mac::MacInterface
+{
+	typedef uint16_t Address;
+
+	static const Address BROADCAST_ADDRESS=255;
+
+	struct Message
+	{
+		uint8_t size;
+		Address dstAddress;
+		uint8_t payload[126];
+		Error state;
+	};
+
+	Delegate<Message&> txCompleteDelegate;
+
+	template<void (*f)(Message& msg)>
+	struct TxCompleteCallback{};
+
+	Error send(Message& msg)
+	{
+		txCompleteDelegate(msg);
+		return armarow::common::SUCCESS;
+	}
+
+	template<void (*f)(Message&)>
+	Error setAttribute(const TxCompleteCallback<f>& attr)
+	{
+		txCompleteDelegate.bind<f>();
+		return armarow::common::SUCCESS;
+	}
 };
 
-typedef armarow::mac::MacCsmaCa<My_MAC_Config,platform::config::rc_t,armarow::mac::Enable> Mac_Layer;
-Mac_Layer mac;
+typedef TestMAC MacLayer;
+MacLayer mac;
 
-void transmission_completed_callback(){
+typedef MacLayer::Message Message;
+Message msg;
 
-    //secure callback, so it can't be interrupted
+PGM_P content="MAC LAYER TEST";
+
+void txComplete(Message& msg){
     avr_halib::locking::GlobalIntLock lock;
-    ::logging::log::emit()
-        << PROGMEMSTRING("msg...") << (int) mac.get_result_of_last_send_operation()
-        << ::logging::log::endl;
-    if(mac.get_result_of_last_send_operation()!=0)
-    ::logging::log::emit()
-        << PROGMEMSTRING("ERROR: couldn't transmit last message...") 
-        << ::logging::log::endl;
+
+    log::emit() << "transmission finished with " << msg.state << log::endl;
 }
 
-void async_sending_test(armarow::MAC::mob_t msg){
-  mac.onSend_Operation_Completed_Delegtate.bind<transmission_completed_callback>();
-  int ret;
-  bool verbose = false;
-  delay_ms(500); //wait a little, because the sniffer has to be up earlier, otherwise it cannot receive our data
+void init()
+{
+    strcpy_P(reinterpret_cast<char*>(msg.payload), content);
 
-  while(1){
-    ret=mac.send(msg);
-    if(ret==0){
-    //delay_ms(1000);
-    }
-    if(verbose){
-        avr_halib::locking::GlobalIntLock lock;
-        ::logging::log::emit()
-            << PROGMEMSTRING("ret is: ") << ret
-            << ::logging::log::endl;
-    }
-     //delay_ms(100);
-  }
+    msg.size        = strlen_P(content);
+    msg.dstAddress = MacLayer::BROADCAST_ADDRESS;
+
+	MacLayer::TxCompleteCallback<&txComplete> txCompleteCallback;
+
+	Error error=mac.setAttribute(txCompleteCallback);
+	if(error)
+		log::emit<log::Error>() << "setting of callback failed: " << error << log::endl;
+    sei();
 }
 
-/*! Test class for object storage feature. */
-struct My_Information{
 
-    int messwert1;
-    int messwert2;
-    int messwert3;
+int main()
+{
+	init();
 
-    My_Information(){
-        messwert1=10;
-        messwert2=20;
-        messwert3=30;
-    }
+	log::emit() << PROGMEMSTRING("Mac Bubbler") << log::endl;
 
-    void print(){
-        ::logging::log::emit() << "messwert1: " << messwert1 << ::logging::log::endl;
-        ::logging::log::emit() << "messwert2: " << messwert2 << ::logging::log::endl;
-        ::logging::log::emit() << "messwert3: " << messwert3 << ::logging::log::endl;
-    }
-} a;
-
-/* === main ================================================================= */
-int main() {
-    uint16_t cnt = 0;
-    armarow::mac::mob_t messageobject;
-   //try out different message sizes
-   //const char messagecontent[] = "MAC LAYER TEST";  //15 bytes
-   //const char messagecontent[] = "MAC LAYER TEST with much larger messages, increasing efficency"; //63 bytes
-   const char messagecontent[] = "MAC LAYER TEST with larger messages, increasing efficency, using the maximum message size, to test for collisions"; //114 bytes
-
-    //init message with your data and set destination 
-    for(unsigned int i=0;i<sizeof(messagecontent);i++)
-        messageobject.payload[i]=messagecontent[i];   //copy data in message payload
-
-    messageobject.size=sizeof(messagecontent);
-    //set destination
-    messageobject.header.dest_adress = 20; //armarow::MAC::MAC_BROADCAST_ADRESS;
-    messageobject.header.dest_pan = 0;
-    messageobject.header.controlfield.ackrequest = 1;//1;
-    /* //example for usage of object storage functionality
-    a.messwert1=40;
-    a.messwert2=50;
-    a.messwert3=60;
-
-    ::logging::log::emit() << "Object a: " << ::logging::log::endl;
-    a.print();
-
-    messageobject.store_object_in_payload(a);
-
-    My_Information b;
-
-    ::logging::log::emit() << "Object b before: " << ::logging::log::endl;
-    b.print();
-
-    messageobject.get_object_from_payload(b);
-
-    :logging::log::emit() << "Object b after: " << ::logging::log::endl;
-    b.print();
-
-    //end of example
-    */
-    sei();                              // enable interrupts
-    ::logging::log::emit()
-        << PROGMEMSTRING("Starting bubbler (repeated send of the same message)!")
-        << ::logging::log::endl << ::logging::log::endl;
-
-    async_sending_test(messageobject);
+    while(true)
+	{
+		Error error=mac.send(msg);
+		if(error)
+			log::emit<log::Error>() << "transmission rejected due to " << error << log::endl;
+		delay_ms(500);
+	}
+	return 0;
 }
 
---
+++ include/armarow/common/error.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "platform-cfg.h"
+
+namespace armarow {
+namespace common {
+
+	/** \brief central definition of all possible error codes
+	 *
+	 *  this enumeration has to be updated by the implementer of new functionality
+	 **/
+	enum Error
+	{
+		SUCCESS = 0 ,   /**< operation successfull **/
+		FAILURE     ,   /**< operation failed **/
+		BUSY	    ,   /**< medium or hardware busy **/
+		OUT_OF_RANGE   /**< parameter not valid for this attribute **/
+	};
+}
+}
+
+/** \brief specially overloaded operator to print readable strings of error codes
+ *
+ *  \param out the logging instance to report the error string to
+ *  \param error the error the should be logged
+ *  \return the logging instance passed as parameter
+ **/
+::LoggingOutput& operator<<(::LoggingOutput& out, const armarow::common::Error error)
+{
+	switch(error)
+	{
+		case(armarow::common::SUCCESS):      return out << PROGMEMSTRING("ok");
+		case(armarow::common::FAILURE):      return out << PROGMEMSTRING("operation failed");
+		case(armarow::common::BUSY):         return out << PROGMEMSTRING("medium or hardware busy");
+		case(armarow::common::OUT_OF_RANGE): return out << PROGMEMSTRING("parameter out of range");
+	}
+	return out << PROGMEMSTRING("unknown error");
+}
