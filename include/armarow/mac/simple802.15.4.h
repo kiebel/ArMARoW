2119aacc6e1b13fe7c80a94ddd030a8cf38e8dd2
Christoph Steup <steup@ivs.cs.ovgu.de>
2011-12-06 08:59:05 +0100

    Tried to combine old mac layer code with new api
        TODO: test compilation
        TODO: fix compilation errors

--- include/armarow/mac/simple802.15.4.h
+++ include/armarow/mac/simple802.15.4.h
@@ -1,3 +1,111 @@
-#include "new.h"
-#include "MediumAccessLayer.h"
+#include <armarow/common/interface.h>
 #include "simple802.15.4/csma_ca.h"
+#include "simple802.15.4/message.h"
+
+namespace armarow {
+namespace mac {
+
+    /** \brief A simplified IEEE 802.15.4 MAC-Protocol
+     *
+     *  \tparam config structure containing all the configuration parameters of this mac protocol
+     *  \tparam PhysicalLayer the basic layer of this protocol
+     *  
+     *  This MAC-Protocol only support the non-beacon mode.
+     *
+     *  Currently implemented are the fallowing features:
+     *      - CSMA/CA based transmission
+     ***/
+    template<typename config, class PhysicalLayer>
+    class Simple802_15_4 : public common::Interface< simple802.15.4::Message<config> >, public PhysicalLayer
+    {
+        public:
+            /**\brief forward typedef of message type**/
+            typedef simple802.15.4::Message<config> Message;
+
+        private:
+            /**\brief the CSMA_CA implementation**/
+            NonBeaconCSMA_CA<config, PhysicalLayer, Message> mac;
+            /**\brief internal variable for sequence number generation**/
+            Message::SequenceNumberType seqNumber;
+
+            void txComplete(Message& msg)
+            {
+                this->txCompleteCallback(msg);
+            }
+
+            /** \brief check a message against the protocol specification
+             *
+             *  \param msg a reference to the message to be checked
+             *  \return boolean indicating the validity of the message
+             **/
+            bool isValid(Message& msg) const
+            {
+                return true;
+            }
+
+        public:
+            /**\brief transmit a message
+             *
+             * \param msg a reference to the message being transmitted
+             *
+             * This function uses the user supplied messsage as a
+             * buffer for storing the message as well as further
+             * information. The user has to make sure to not alter
+             * or delete the buffer during the transmission or
+             * inconsistencies are quite possible. Read access
+             * however is no problem. If the user did not register
+             * the txCompleteCallback, it is possible to infer the
+             * end of the transmission by polling the state
+             * variable of the message. The operation is finished
+             * when the variable changes from BUSY to SUCCESS or
+             * FAILURE.
+             *
+             **/
+            common::Error send(Message& msg)
+            {
+                if(mac.txReady())
+                {
+                                msg.seqNumber      = seqNumber++;
+                                msg.source.address = config::address;
+                                mag.source.pan     = config::pan;
+                    return mac.transmit(msg, *this);
+                }
+                else
+                                return common::BUSY;
+            }
+
+            /**\brief receive a message
+             *
+             * \param msg a reference to the message buffer to be filled with the received message
+             *
+             * This function uses the user supplied messsage as a
+             * buffer for storing the message as well as further
+             * information. The implementation will overwrite any
+             * content in the buffer during the reception.  This
+             * function returns with NO_MESSAGE if there is no
+             * message to be received. To support polling usage of
+             * the mac protocol.  If the user want`s to be notified
+             * on arrival of a new message, he should register the
+             * rxCallback.
+             *
+             **/
+            common::Error receive(Message& msg)
+            {
+                common::Error error = this->receive(msg);
+                if(error)
+                    return error;
+                if(!isValid(msg))
+                    return NO_MESSAGE;
+                msg.state=VALID_RX;
+                return SUCCESS;
+            }
+
+            void reset()
+            {
+                this->PhysicalLayer::reset();
+                mac.reset();
+                seqNumber=0;
+            }
+    };
+}
+}
--- include/armarow/mac/simple802.15.4/backoffTimer.h
+++ include/armarow/mac/simple802.15.4/backoffTimer.h
@@ -1,4 +1,7 @@
-UseInterrupt(SIG_OUTPUT_COMPARE3A);
+#include <math.h>
+#include <avr-halib/share/eggTimer.h>
+
+#include <platform-cfg.h>
 
 namespace armarow {
 namespace mac {
@@ -6,79 +9,97 @@ namespace simple802_15_4
 {
 
     /** \brief Timer abstraction encapsulating the IEEE 801.15.4 CSMA/CA backoff behaviour.
-     *  \tparam minBackoffExponent minimum Backoff exponent
-     *  \tparam maxBackoffExponent maximum Backoff exponent
-     *  \tparam backoffUnitDuration the duration of one backoff unit in us
-     */
-    template<uint8_t minBackoffExponent, uint8_t maxBackoffExponent, uint16_t backoffUnitDuration>
-    class BackoffTimer {
-    private:
-        /** \brief One-shot-Timer executing the registered callback after timeout duration **/
-        ExactEggTimer<Timer3> timer;
-        /** \brief number of sequent backoffs
-         * is used to calculate the next backoff exponent and by this the number of backoff Periods to wait
-         * see IEEE 802.15.4 7.5.1.4
-         **/
-        uint8_t backoffCount;
+     *  \tparam config configuration structure for backoff timing
+     **/
+    template<typename config>
+    class BackoffTimer{
+        private:
+            /** \brief One-shot-Timer executing the registered callback after timeout duration **/
+            ExactEggTimer<config::Timer> timer;
+            /** \brief number of waited backoffs
+             *
+             * is used to calculate the next backoff exponent and by this the
+             * number of backoff Priods to wait
+             *
+             * see IEEE 802.15.4 7.5.1.4
+             **/
+            uint8_t backoffCount;
 
-    public:
-        /** \brief Default constructor
-         *  \tparam T type of the object on which the callback will be called
-         *  \tparam F member function of T acting as callback
-         *  \param obj instance of T used to call F
-         *
-         *   Resets backoff exponent to minimum value, also
-         *   registers supplied callback function with timer
-         *   delegate
-         **/
-        template<typedef T, void (*T::F)(void)>
-        BackoffTimer(T& obj) {
-            timer.onTimerDelegate<T, F>(obj);
-            reset();
-        }
+            static const uint8_t minBackoffExponent = config::minBackoffExponent;
+            static const uint8_t maxBackoffExponent = config::maxBackoffExponent;
+            static const uint8_t maxBackoffCount    = config::maxBackoffCount;
+            static const uint32_t backoffPeriod      = config::backoffPeriod;
 
-        /** \brief Destructor stopping the internal timer**/
-        ~BackoffTimer() {
-            timer.stop();
-        }
+            /** \brief calculates random backoff time
+             *
+             *   \return backoff waiting time in us
+             *
+             *   As defined in the IEEE 802.15.4 the backoff
+             *   interval containing the potential backoff values
+             *   grows exponentially with the number of waited
+             *   backoffs.
+             **/
+            uint32_t backoffTime()
+            {
+                uint8_t  backoffExponent = min( (minBackoffExponent + backoffCount), 
+                                                 maxBackoffExponent 
+                                              );
 
-        /** \brief calculates random backoff time
-         *   As defined in the IEEE 802.15.4 the backoff
-         *   interval containing the potential backoff values
-         *   grows exponentially with the number of waited
-         *   backoffs.
-         **/
-        static uint16_t getBackoffTime(uint8_t backoffCount)
-        {
-            uint8_t  backoffExponent = min( (minBackoffExponent + backoffCount) , maxBackoffExponent );
-            uint16_t backoffPeriods = (uint32_t)rand() * ((1 << (backoffExponent)) - 1) / (RAND_MAX + 1);
                 // +1 enables the compiler to optimize divison to shift
-            return backoff * backoffUnitDuration / 1000;
-        }
+                uint16_t backoffPeriods =   (uint32_t)rand()
+                                          * ((1 << (backoffExponent)) - 1)
+                                          / (RAND_MAX + 1); 
 
-        /** \brief Start random backoff timer
-         **/
-        void wait() {
-            uint16_t backoffTime = getBackoffTime(backoffCount);
-            timer.start(backoffTime);
-            backoffCount++;
-            log::emit<log::Trace>()
-                << "started backoff timer, timeout in "
-                << backoffTime << " ms." << log::endl;
-        }
-        void shortwait()
-        {
-            uint16_t backoffTime = getBackoffTime(0);
-            timer.start(backoffTime);
-            log::emit<log::Trace>()
-                << "started backoff timer, timeout in "
-                << backoffTime << " ms." << log::endl;
-        }
+                return periodNumber * backoffPeriod;
+            }
 
-        /** \brief Reset the backoff exponent to the minimal value **/
-        void reset() {
-            backoffCount = 0;
-        }
+        public:
+            /** \brief Default constructor
+             *  \tparam T type of the object on which the callback will be called
+             *  \tparam F member function of T acting as callback
+             *  \param obj instance of T used to call F
+             *
+             *   Resets backoff exponent to minimum value, also
+             *   registers supplied callback function with timer
+             *   delegate
+             **/
+            
+            BackoffTimer() {
+                reset();
+            }
+
+            /** \brief Destructor stopping the internal timer**/
+            ~BackoffTimer() {
+                timer.stop();
+            }
+
+            /** \brief Start random backoff timer
+             *
+             *  \return if the maximum number of backoffs was exceeded
+             **/
+            bool wait() {
+                if(backoffCount > maxBackoffCount)
+                    return false;
+
+                timer.start( (uint16_t)(backoffTime() / 1000) );
+
+                log::emit<log::Trace>()
+                    << "started backoff timer, timeout in "
+                    << backoffTime << " ms." << log::endl;
+
+                return true;
+            }
+
+            /** \brief Reset the backoff counter **/
+            void reset() {
+                backoffCount = 0;
+            }
+
+            template<typename T, void (T::*f)(void)>
+            void register(T& obj)
+            {
+                timer.onTimerDelegate.template bind<T, f>(obj);
+            }
     };
 }
 }
