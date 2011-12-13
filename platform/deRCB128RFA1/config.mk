5255f32731f05400e26f47f379038ad610a002f0
Christoph Steup <christoph.steup@st.ovgu.de>
2011-12-13 20:49:31 +0100

    Added new platform deRCB128RFA1
        mostly similar to deRFmega
        changes in logging uart configuration

---
+++ platform/deRCB128RFA1/config.mk
@@ -0,0 +1,5 @@
+ARCH		= avr
+MCU		    = atmega128rfa1
+CLOCK		= 16000000
+PROGRAMMER	?= avr911
+PORT        ?= /dev/ttyS0
---
+++ platform/deRCB128RFA1/radio.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <avr-halib/avr/interrupt.h>
+#include <armarow/phy/atmega128rfa1/atmega128rfa1-rc.h>
+
+UseInterrupt(TRX24_RX_START_vect);
+UseInterrupt(TRX24_RX_END_vect);
+UseInterrupt(TRX24_TX_END_vect);
+
+namespace platform {
+namespace config {
+    using armarow::phy::ATmega128RfA1;
+    using armarow::phy::ATmega128RfA1CFG;
+
+    template<typename Config=ATmega128RfA1CFG>
+    struct RadioDriver : public ATmega128RfA1< Config >{};
+}
+}
