bab4410f875dfd895ad3277fc5aa15d13d4b94c2
Christoph Steup <christoph.steup@st.ovgu.de>
2012-05-16 19:36:32 +0200

    Added debug portmap to deRCB128RFA1 platform

--- platform/deRCB128RFA1/config.mk
+++ platform/deRCB128RFA1/config.mk
@@ -1,12 +1,9 @@
 .PHONY: debug
 
 PLATFORM_DIR := $(dir $(abspath $(lastword ${MAKEFILE_LIST})))
-
-debug:
-	@echo "This makefile is ${PLATFORM_DIR}/config.mk"
-
-INCLUDES += ${PLATFORM_DIR}/include
+INCLUDES     += ${PLATFORM_DIR}/include
 
 include ${ARMAROW_DIR}/make/avr.mk
 
-PLATFORM := ${HALIB_DIR}/platform/deRCB128RFA1
+PLATFORM    := ${HALIB_DIR}/platform/deRCB128RFA1
+PM_SRC_DIRS +=${PLATFORM_DIR}/portmap
